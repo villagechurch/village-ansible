1b283deb4f9fdd85f5f9ea6d6d7067dd63c729ea
Isaac Wiebe <isaacwiebe@me.com>
2020-12-10 15:43:57 -0800

    Add first_run tags and add ceph_fuse to samba and ceph-client

--- cluster.sh
+++ cluster.sh
@@ -1,22 +1,26 @@
 #! /bin/zsh
 
 ARG1=$1
+fflag=1
 
 cd ansible
 
 
-while getopts ":g:d" opt; do
+while getopts ":g:d:a:f" opt; do
   case $opt in
     a)
       echo -e "\n*********\nTargetting ALL hosts in inventory\n*********\n" >&2
-      command="ansible-playbook -i inventory/staging cluster.yml --skip-tags first_run"
+      command="ansible-playbook -i inventory/staging cluster.yml"
       ;;
     g)
       echo -e "\n*********\nTargetting $OPTARG hosts in inventory\n*********\n" >&2
-      command="ansible-playbook -l $OPTARG -i inventory/staging cluster.yml --skip-tags first_run"
+      command="ansible-playbook -l $OPTARG -i inventory/staging cluster.yml"
       ;;
     d)
       dflag=1
+      ;;
+    f)
+      fflag=0
       ;;
     \?)
       echo -e "\n*********\nTargetting All hosts in inventory\n*********\n" >&2
@@ -24,6 +28,12 @@ while getopts ":g:d" opt; do
       ;;
   esac
 done
+
+echo "$fflag"
+
+if [ "$fflag" -gt 0 ]; then
+    command+=" --skip-tags first_run"
+fi
 
 if [ ! -z "$dflag" ]; then
     command+=" --check"
