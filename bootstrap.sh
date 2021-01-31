53f591b212c4390c69a2bde872d1c2acb127f79e
Isaac Wiebe <isaacwiebe@me.com>
2021-01-31 07:24:05 -0800

    Update bootstrap process

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,32 @@
 #! /bin/zsh
 
+ARG1=$1
+rflag=1
+
 cd ansible
 
-#ansible-playbook -i inventory/bootstrap bootstrap.yml
-ansible-playbook -i inventory/bootstrap bootstrap.yml --ask-pass
+#default command, barring any cli arguments
+command="ansible-playbook -l bootstrap -i inventory/bootstrap bootstrap.yml"
+
+while getopts ":cr" opt; do
+  case $opt in
+    c)
+      echo -e "\n*********\nTargetting cloud hosts in inventory\n*********\n" >&2
+      command="ansible-playbook -l bootstrap_cloud -i inventory/bootstrap bootstrap.yml"
+      ;;
+    r)
+      rflag=0
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG"
+      exit 2
+      ;;
+  esac
+done
+
+if [ "$rflag" -gt 0 ]; then
+    command+=" --ask-pass"
+fi
+
+echo -e "*********\n$command\n*********\n" >&2
+eval "$command"
